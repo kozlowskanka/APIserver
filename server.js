4508224cba8d16bc7445bb445b9fabcc1757acab
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-12 20:04:04 +0200

    Add frontend files

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 
+const app = express();
 const testimonialRoutes = require('./routes/testimonials.routes.js');
 const concertsRoutes = require('./routes/concerts.routes.js');
 const seatsRoutes = require('./routes/seats.routes.js');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use('/api', testimonialRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
-
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 })
-
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
