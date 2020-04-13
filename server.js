deb0c2a319ac68b18e829e75eebffed6d3c2480b
Anka Kozłowska <kozlowskanka@gmail.com>
2020-04-13 17:28:10 +0200

    Prepared for  deploymen

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
+
 const testimonialRoutes = require('./routes/testimonials.routes.js');
 const concertsRoutes = require('./routes/concerts.routes.js');
 const seatsRoutes = require('./routes/seats.routes.js');
@@ -9,12 +11,19 @@ const seatsRoutes = require('./routes/seats.routes.js');
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', testimonialRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 })
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
